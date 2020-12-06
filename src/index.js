641ec876d438db3c7109fcb7fdaaca0750a05c59
tiapnn <tiapnn@yahoo.it>
2020-12-06 10:58:13 +0100

    addded random quotes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import '/sass/main.scss';
 // // preloader 
 // const preloaderTL = gsap.timeline();
 // preloaderTL.to('#logo', {yPercent: -20, opacity: 0, delay: 4})
-// preloaderTL.to('.preloader', {transform: 'scaleY(0)', transformOrigin: 'top', delay: 10})
+// preloaderTL.to('.preloader', {transform: 'scaleY(0)', transformOrigin: 'top', delay: 1})
 
 // open all social links in new tab
 let socialLinks = document.querySelectorAll('.social-links--box a');
@@ -81,27 +81,34 @@ function viewProj(proj) {
     }
     
 }
+
+const quote = document.querySelector('q')
+const timer = 5000
+
 function getQuotes () {
     fetch('https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json')
         .then(response => response.json())
-        .then(data => { data.data})
+        .then(data => setInterval(() => shuffleQuotes(data.data), timer))
+}
+getQuotes()
+
+function shuffleQuotes(allQuotes) {
+    var randomQuote = getRandomQuote(allQuotes)
+    changeQuote(randomQuote)
 }
 
-
-
-const quote = document.querySelector('q')
-
-function changeQuote(quote) {
-    const allQuotes = getQuotes()
-    console.log(quote);
-    console.log(allQuotes);
+function getRandomQuote(allQuotes) {
+    return allQuotes[Math.floor(Math.random()*allQuotes.length)]
 }
 
-changeQuote(quote)
+function changeQuote(q) {
+    quote.innerHTML = q.quote
+    quote.cite = q.source
+}
 
 
 //message to all the developers out there
-const message = "Nice to see you around here! Send me a message if you want to reuse my portfolio template or some explanations of any feature in it! Cheers!" 
+const message = "Nice to see you around here! Send me a message if you want to reuse my portfolio template or you want explanations of any features! Cheers!" 
 console.group("Hey developer!")
 console.log(message)
 console.log("%cMattia P - tiapnn@yahoo.it", 'font-size:20px');
