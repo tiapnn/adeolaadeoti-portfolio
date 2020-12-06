a8b6e0015534e555dea93ebc3c3e98732e13de07
tiapnn <tiapnn@yahoo.it>
2020-12-06 22:34:48 +0100

    finished quote animation and added gif for canary

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import '/sass/main.scss';
 // preloaderTL.to('#logo', {yPercent: -20, opacity: 0, delay: 4})
 // preloaderTL.to('.preloader', {transform: 'scaleY(0)', transformOrigin: 'top', delay: 1})
 
-// open all social links in new tab
-let socialLinks = document.querySelectorAll('.social-links--box a');
-socialLinks.forEach(link => {
-    link.target = '_blank';
-});
-
 
 // custom cursor
 // const cursor = document.querySelector('.cursor');
@@ -83,7 +77,7 @@ function viewProj(proj) {
 }
 
 const quote = document.querySelector('q')
-const timer = 5000
+const timer = 6000
 
 function getQuotes () {
     fetch('https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json')
@@ -94,6 +88,7 @@ getQuotes()
 
 function shuffleQuotes(allQuotes) {
     var randomQuote = getRandomQuote(allQuotes)
+    toggleTransition()
     changeQuote(randomQuote)
 }
 
@@ -102,8 +97,26 @@ function getRandomQuote(allQuotes) {
 }
 
 function changeQuote(q) {
-    quote.innerHTML = q.quote
-    quote.cite = q.source
+    setTimeout(() => {
+        quote.innerHTML = q.quote
+        quote.cite = q.source
+    }, 300);
+}
+
+
+function toggleTransition() {
+    // trigger toggleTransition method
+    quote.removeEventListener("transitionend", onTransitionEnd);
+    quote.addEventListener("transitionend", onTransitionEnd);
+  
+    // removing the class for leave state
+    quote.classList.remove("fade");
+}
+
+function onTransitionEnd() {
+    // removing the listener again so that it is triggered only once
+    quote.removeEventListener("transitionend", onTransitionEnd)
+    quote.classList.add("fade")
 }
 
 
@@ -113,3 +126,5 @@ console.group("Hey developer!")
 console.log(message)
 console.log("%cMattia P - tiapnn@yahoo.it", 'font-size:20px');
 console.groupEnd()
+
+  
