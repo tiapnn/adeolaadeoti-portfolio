a5ab316584c731c3acfae37b52e6b39f59197f7e
tiapnn <tiapnn@yahoo.it>
2020-12-06 08:35:22 +0100

    style fixes

--- src/index.js
+++ src/index.js
@@ -81,3 +81,28 @@ function viewProj(proj) {
     }
     
 }
+function getQuotes () {
+    fetch('https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json')
+        .then(response => response.json())
+        .then(data => { data.data})
+}
+
+
+
+const quote = document.querySelector('q')
+
+function changeQuote(quote) {
+    const allQuotes = getQuotes()
+    console.log(quote);
+    console.log(allQuotes);
+}
+
+changeQuote(quote)
+
+
+//message to all the developers out there
+const message = "Nice to see you around here! Send me a message if you want to reuse my portfolio template or some explanations of any feature in it! Cheers!" 
+console.group("Hey developer!")
+console.log(message)
+console.log("%cMattia P - tiapnn@yahoo.it", 'font-size:20px');
+console.groupEnd()
