25e525778f38e229415962bbe01cbd96906011a9
tiapnn <tiapnn@yahoo.it>
2020-12-12 16:58:47 +0100

    changes on project section

--- src/index.js
+++ src/index.js
@@ -43,41 +43,23 @@ allLinks.forEach(link => {
 
 
 const projs = document.querySelectorAll('#projects .project-box')
-const contents = document.querySelectorAll("#projects .project-box__content")
 const links = document.querySelectorAll("#projects .project-box__link")
 
 projs.forEach( proj => {
     proj.onclick = () => viewProj(proj) 
 })
 
-function resetProjs() {
-    projs.forEach(p => p.style.height = "35rem")
-    contents.forEach(c => c.style.opacity = "1")
-    links.forEach(l => l.classList.remove("shaky"))
-}
 
 function viewProj(proj) {
-    let content = proj.querySelector("#projects .project-box__content")
     let link = proj.querySelector("#projects .project-box__link")
-
-    if (proj.style.height == "60rem")
-    {
-        proj.style.height = "35rem" 
-        content.style.opacity = "1"
-        link.classList.remove("shaky")
-    } 
-    else 
-    {
-        resetProjs()
-        proj.style.height = "60rem"
-        content.style.opacity = "0"
+    link.classList.remove("shaky")
+    setTimeout(() => {
         link.classList.add("shaky")
-    }
-    
+    }, 200);
 }
 
 const quote = document.querySelector('q')
-const timer = 5000
+const timer = 6000
 
 function getQuotes () {
     fetch('https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json')
