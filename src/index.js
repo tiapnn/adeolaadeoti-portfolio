78742fc4f6d43a834fb64b7b396f4bbe3dc9bffb
tiapnn <tiapnn@yahoo.it>
2021-04-05 21:57:42 +0200

    perf: format index.html

--- src/index.js
+++ src/index.js
@@ -1,139 +1,148 @@
-import '/sass/main.scss';
+import "/sass/main.scss";
 
-// // preloader 
+// // preloader
 // const preloaderTL = gsap.timeline();
 // preloaderTL.to('#logo', {yPercent: -20, opacity: 0, delay: 4})
 // preloaderTL.to('.preloader', {transform: 'scaleY(0)', transformOrigin: 'top', delay: 1})
 
-
 // custom cursor
 // const cursor = document.querySelector('.cursor');
 // window.onmousemove = (e) => {
-//     cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`)    
+//     cursor.setAttribute('style', `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`)
 // }
 
 // navigation
-const tl = gsap.timeline({paused:true, reversed: true});
-tl.to('.box', {height: '100vh', duration: .3, transformOrigin: 'bottom', stagger: .2})
-tl.to('.nav-main__content', {opacity: '1', visibility: 'visible', yPercent: -5, duration: .3, transformOrigin: 'bottom', stagger: .2})
+const tl = gsap.timeline({ paused: true, reversed: true });
+tl.to(".box", {
+  height: "100vh",
+  duration: 0.3,
+  transformOrigin: "bottom",
+  stagger: 0.2,
+});
+tl.to(".nav-main__content", {
+  opacity: "1",
+  visibility: "visible",
+  yPercent: -5,
+  duration: 0.3,
+  transformOrigin: "bottom",
+  stagger: 0.2,
+});
 
-const navIcon = document.querySelector('.nav-icon');
-navIcon.onclick = function() {
-    if (tl.reversed()) {
-        this.classList.add('nav-anim')
-        tl.play()
-        document.body.classList.add('noScroll');
-    } else {
-        this.classList.remove('nav-anim')
-        tl.reverse()
-        document.body.classList.remove('noScroll');
-    }
-}
+const navIcon = document.querySelector(".nav-icon");
+navIcon.onclick = function () {
+  if (tl.reversed()) {
+    this.classList.add("nav-anim");
+    tl.play();
+    document.body.classList.add("noScroll");
+  } else {
+    this.classList.remove("nav-anim");
+    tl.reverse();
+    document.body.classList.remove("noScroll");
+  }
+};
 
-const allLinks = document.querySelectorAll('.list__item a');
+const allLinks = document.querySelectorAll(".list__item a");
 
-allLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('noScroll');
-        tl.reverse()
-        navIcon.classList.remove('nav-anim')
-    })
-})
+allLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("noScroll");
+    tl.reverse();
+    navIcon.classList.remove("nav-anim");
+  });
+});
 
+const projs = document.querySelectorAll("#projects .project-box");
+const links = document.querySelectorAll("#projects .project-box__link");
 
-const projs = document.querySelectorAll('#projects .project-box')
-const links = document.querySelectorAll("#projects .project-box__link")
-
-projs.forEach( proj => {
-    proj.onclick = () => viewProj(proj) 
-})
-
+projs.forEach((proj) => {
+  proj.onclick = () => viewProj(proj);
+});
 
 function viewProj(proj) {
-    let link = proj.querySelector("#projects .project-box__link")
-    link.classList.remove("shaky")
-    setTimeout(() => {
-        link.classList.add("shaky")
-    }, 200);
+  let link = proj.querySelector("#projects .project-box__link");
+  link.classList.remove("shaky");
+  setTimeout(() => {
+    link.classList.add("shaky");
+  }, 200);
 }
 
-const quote = document.querySelector('q')
-const timer = 6000
+const quote = document.querySelector("q");
+const timer = 6000;
 
-function getQuotes () {
-    fetch('https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json')
-        .then(response => response.json())
-        .then(data => setInterval(() => shuffleQuotes(data.data), timer))
+function getQuotes() {
+  fetch(
+    "https://gist.githubusercontent.com/tiapnn/ca5f70fc803eef6c02ded745ad624c71/raw/9b2c6f5440785d7b62ee04953d5a779c3ed8b166/programming-quotes.json"
+  )
+    .then((response) => response.json())
+    .then((data) => setInterval(() => shuffleQuotes(data.data), timer));
 }
-getQuotes()
+getQuotes();
 
 function shuffleQuotes(allQuotes) {
-    let randomQuote = getRandomQuote(allQuotes)
-    toggleTransition()
-    changeQuote(randomQuote)
+  let randomQuote = getRandomQuote(allQuotes);
+  toggleTransition();
+  changeQuote(randomQuote);
 }
 
 function getRandomQuote(allQuotes) {
-    return allQuotes[Math.floor(Math.random()*allQuotes.length)]
+  return allQuotes[Math.floor(Math.random() * allQuotes.length)];
 }
 
 function changeQuote(q) {
-    setTimeout(() => {
-        quote.innerHTML = q.quote
-        quote.cite = q.source
-    }, 300);
+  setTimeout(() => {
+    quote.innerHTML = q.quote;
+    quote.cite = q.source;
+  }, 300);
 }
 
-
 function toggleTransition() {
-    // trigger toggleTransition method
-    quote.removeEventListener("transitionend", onTransitionEnd);
-    quote.addEventListener("transitionend", onTransitionEnd);
-  
-    // removing the class for leave state
-    quote.classList.remove("fade");
+  // trigger toggleTransition method
+  quote.removeEventListener("transitionend", onTransitionEnd);
+  quote.addEventListener("transitionend", onTransitionEnd);
+
+  // removing the class for leave state
+  quote.classList.remove("fade");
 }
 
 function onTransitionEnd() {
-    // removing the listener again so that it is triggered only once
-    quote.removeEventListener("transitionend", onTransitionEnd)
-    quote.classList.add("fade")
+  // removing the listener again so that it is triggered only once
+  quote.removeEventListener("transitionend", onTransitionEnd);
+  quote.classList.add("fade");
 }
-
 
 //scroll anchor links fixed
 let switchView = function (hash = location.hash, adjust = 350) {
+  try {
+    let mobileView = window.matchMedia("(max-width: 426px)");
+    if (mobileView.matches) adjust = 250;
+    let elem = document.querySelector(hash);
+    let top = elem.offsetTop;
+    window.scrollTo(0, top - adjust);
+  } catch (DOMException) {
+    location.hash = "";
+  }
+};
 
-    try {
-        let mobileView = window.matchMedia("(max-width: 426px)")
-        if (mobileView.matches) adjust = 250
-        let elem = document.querySelector(hash);
-        let top = elem.offsetTop
-        window.scrollTo(0, top - adjust)
-    } catch (DOMException) {    
-        location.hash = "";
-    } 
-}
+document.addEventListener("DOMContentLoaded", () => {
+  switchView();
+});
+window.onhashchange = () => {
+  switchView();
+};
 
-document.addEventListener('DOMContentLoaded', () => { switchView() });
-window.onhashchange = () => { switchView() }
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-
-    anchor.onclick = (e) => {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.onclick = (e) => {
     let target = e.target;
-    e.preventDefault()
-    switchView(target.attributes.href.value)
-    
-    }
-})
-
+    e.preventDefault();
+    switchView(target.attributes.href.value);
+  };
+});
 
 //message to all the developers out there
-const message = "Nice to see you around here! Send me a message if you want to reuse my portfolio template or you want explanations of any features! Cheers!" 
-console.group("Hey developer!")
-console.log(message)
-console.log("%cMattia P - tiapnn@yahoo.it", 'font-size:20px');
-console.log("%c🍝🥘", 'font-size:100px');
-console.groupEnd()
-
+const message =
+  "Nice to see you around here! Send me a message if you want to reuse my portfolio template or you want explanations of any features! Cheers!";
+console.group("Hey developer!");
+console.log(message);
+console.log("%cMattia P - tiapnn@yahoo.it", "font-size:20px");
+console.log("%c🍝🥘", "font-size:100px");
+console.groupEnd();
